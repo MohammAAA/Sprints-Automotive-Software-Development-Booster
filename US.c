14d4b4fbb3c9ab5de0ba655846d45f5e986659dc
Mohammed Abdelalim <MohamedLemo14@gmail.com>
2020-02-05 15:48:25 +0200

    Update US.c
    added full functionality of the two ultrasonics (v.1)

--- US.c
+++ US.c
@@ -10,7 +10,10 @@ volatile uint16 overflows_0;
 volatile uint16 overflows_2;
 volatile uint8 flag = 1; //positive edge trigger indicator
 volatile uint8 flag_timer = 0; // if =0 so timer 0 is used ,, if =2 then timer 2 is used
-volatile uint16 Distance =0;
+volatile uint16 Distance_Front =0;
+volatile uint16 Distance_Side =0;
+
+volatile uint8 US_Number =0;
 
 ISR (__vector_3);
 
@@ -26,6 +29,7 @@ void US_Init(uint8 Timer_Select)
 void US_Start (uint8 US_Select){
 	switch (US_Select){
 		case US_FRONT:
+		US_Number = US_FRONT;
 		GPIO_Set_Value(PORTA,1, HIGH); // sending 10us pulse
 		SETBIT(MCUCSR,6); // configuring INT2 to be +ve edge triggered
 		_delay_ms(200);
@@ -34,6 +38,7 @@ void US_Start (uint8 US_Select){
 		break;
 			
 		case US_SIDE:
+		US_Number = US_SIDE;
 		GPIO_Set_Value(PORTA,6, HIGH); // sending 10us pulse
 		SETBIT(MCUCSR,6); // configuring INT2 to be +ve edge triggered
 		_delay_ms(200);
@@ -65,6 +70,7 @@ ISR (__vector_3)
 			case TIMER_2:
 			flag = 0;
 			TCNT2 = 0x00;
+			Timer_Set_Prescaler(TIMER_2, PRE_1024);
 			CLRBIT(MCUCSR,6);  // configuring INT2 to be -ve edge triggered
 			SETBIT(GIFR,5); //INTF2 is set to be able to get a new interrrupt
 			break;
@@ -80,7 +86,14 @@ ISR (__vector_3)
 			Timer_Set_Prescaler(TIMER_0, PRE_0);
 			current_count = (TCNT0);   //GPIO_Get_Reg(TCNT0);
 			counts = current_count + (256*overflows_0);
-			Distance = (34000 * PRE_1024*counts)/16000000;
+			switch (US_Number){
+				case US_FRONT:	
+				Distance_Front = (34000 * PRE_1024*counts)/32000000;
+				break;
+				case US_SIDE:
+				Distance_Side = (34000 * PRE_1024*counts)/32000000;
+				break;
+			}
 			overflows_0 = 0;
 			TCNT0 = 0x00;
 			flag =1;
@@ -88,9 +101,17 @@ ISR (__vector_3)
 			break;
 
 			case TIMER_2:
+			Timer_Set_Prescaler(TIMER_2, PRE_0);
 			current_count = (TCNT2);
 			counts = current_count + (256*overflows_2);
-			Distance = (34000 * PRE_256*counts)/16000000;
+			switch (US_Number){
+			case US_FRONT:	
+			Distance_Front = (34000 * PRE_1024*counts)/32000000;
+			break;
+			case US_SIDE:
+			Distance_Side = (34000 * PRE_1024*counts)/32000000;
+			break;
+			}
 			overflows_2 = 0;
 			TCNT2 = 0x00;
 			flag =1;
