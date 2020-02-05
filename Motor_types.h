15e39aeb70a8afec52a71e12b763b1b061e651c4
Mohammed Abdelalim <MohamedLemo14@gmail.com>
2020-02-05 16:20:05 +0200

    Update Motor_types.h
    reset to the previous state

--- Motor_types.h
+++ Motor_types.h
@@ -24,14 +24,6 @@
 
 #define MAX_SPEED 255
 
-void Motor_Init (void);
-void Motor_State (uint8 Motor_n, uint8 Motor_state);
-void Motor_Speed_Control (uint8 Motor_n, uint16 Speed);
-void Motor_Turn_Left(void);
-void Motor_Turn_Right(void);
-void Motor_Move_Forward(void);
-void Motor_Move_Backward(void);
-
 
 
 
